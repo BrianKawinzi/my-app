08bd009be2d858268081b41bc89e2f73e19c6911
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-06-16 21:46:18 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+
+import { Component, HostBinding } from '@angular/core';
+
+import{
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    //animation triggers go here
+  ]
 })
 export class AppComponent {
   title = 'my-app';
